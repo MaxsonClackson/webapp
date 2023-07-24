5661433bfb9305c57a2a8a8f65a684223ab52898
max <whymail@blyaty.gg>
2023-07-24 13:49:08 +0300

    Обновлен html, css, js

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-function addFilterExterior(){}
+function loadSettingField(element) {
+    var container = document.getElementsByClassName('container')[0];
+    if (container.children.length != 0) {
+        while (container.firstElementChild){
+            container.firstElementChild.remove();
+        }
+    }
+    exterior_list_html_id = {'exterior_all': 'All', 'exterior_FN': 'Factory New', 'exterior_MW': 'Minimal Wear',
+     'exterior_FT': 'Field Tested', 'exterior_WW': 'Well Worn', 'exterior_BS': 'Battle Scarred'};
+    for (var key in exterior_list_html_id) {
+        var value = exterior_list_html_id[key]
+        div = document.createElement('div')
+        div.classList.add('setting-bubble')
+        div.id = key
+        div.textContent = value
+        container.appendChild(div)
+    }
+}
+
 
 function filterButtonsEventHandler(event){
     var element = event.target
@@ -8,6 +26,7 @@ function filterButtonsEventHandler(event){
         var block = blocks[i];
         if (element.id == block.id) {
             element.classList.add('on');
+            if (element.id == 'exterior_btn') loadSettingField(element);
         }
         else {
             if (block.className.includes('on')) {
