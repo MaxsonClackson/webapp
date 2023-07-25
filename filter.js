9b54ab6a68837fba9ac29bbd5442fcea5c149540
max <whymail@blyaty.gg>
2023-07-25 20:13:31 +0300

    Откатить сюда если вдруг что

--- filter.js
+++ filter.js
@@ -1,15 +1,17 @@
-function loadSettingField(element) {
+function loadSettingField(element, dict_setting) {
     var container = document.getElementsByClassName('container')[0];
     if (container.children.length != 0) {
         while (container.firstElementChild){
             container.firstElementChild.remove();
         }
     }
-    exterior_list_html_id = {'exterior_all': 'All', 'exterior_FN': 'Factory New', 'exterior_MW': 'Minimal Wear',
-     'exterior_FT': 'Field Tested', 'exterior_WW': 'Well Worn', 'exterior_BS': 'Battle Scarred'};
-    for (var key in exterior_list_html_id) {
-        var value = exterior_list_html_id[key];
-        div = document.createElement('div');
+    var h3 = document.createElement('h3');
+    h3.classList.add('setting-title');
+    h3.textContent = element.textContent;
+    container.appendChild(h3);
+    for (var key in dict_setting) {
+        var value = dict_setting[key];
+        var div = document.createElement('div');
         div.classList.add('setting-bubble');
         div.id = key;
         div.textContent = value;
@@ -43,7 +45,23 @@ function filterButtonsEventHandler(event){
         var block = blocks[i];
         if (element.id == block.id) {
             element.classList.add('on');
-            if (element.id == 'exterior_btn') loadSettingField(element);
+            if (element.id == 'exterior_btn') {
+                exterior_dict_html_id = {'exterior_all': 'All', 'exterior_FN': 'Factory New',
+                'exterior_MW': 'Minimal Wear', 'exterior_FT': 'Field Tested', 'exterior_WW': 'Well Worn',
+                'exterior_BS': 'Battle Scarred'};
+                loadSettingField(element, exterior_dict_html_id);
+                }
+            else if (element.id == 'quality_btn') {
+                quality_dict_html_id = {'quality_cover': 'Covert/Тайное',
+                                        'quality_classified': 'Classified/Засекреченное',
+                                        'quality_restricted': 'Restricted/Запрещенное',
+                                        'quality_milspec': 'Mil-Spec/Армейское',
+                                        'quality_industrial': 'Industrial/Промышленное',
+                                        'quality_consumer': 'Consumer/Ширпотреб',
+                                        };
+                loadSettingField(element, quality_dict_html_id);
+            }
+
         }
         else {
             if (block.className.includes('on')) {
