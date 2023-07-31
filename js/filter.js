212b2c72bd5591d3fa99edbeb7067c06d111e01a
max <whymail@blyaty.gg>
2023-07-31 18:58:56 +0300

    что-то изменил

--- js/filter.js
+++ js/filter.js
@@ -1,6 +1,7 @@
 FiltersValues = {
     choose_collection: null,
     collection_weapons: null,
+    items: null,
     exterior: null,
     quality: null,
     items: null,
@@ -27,19 +28,46 @@ function loadBubbleSettingFieldHTML(element, dict_setting) {
     }
 }
 
-function loadExteriorSettingHTML(element) {
-    var exterior_dict_html_id = {'exterior_all': 'All', 'exterior_FN': 'Factory New', 'exterior_MW': 'Minimal Wear', 'exterior_FT': 'Field Tested', 'exterior_WW': 'Well Worn', 'exterior_BS': 'Battle Scarred'};
-    loadBubbleSettingFieldHTML(element, exterior_dict_html_id);
+function loadExteriorSettingHTML() {
+    var exterior = FiltersValues.exterior
+    var dict_setting = {'exterior_all': 'All', 'exterior_FN': 'Factory New', 'exterior_MW': 'Minimal Wear', 'exterior_FT': 'Field Tested', 'exterior_WW': 'Well Worn', 'exterior_BS': 'Battle Scarred'};
+    $('.container').empty();
+    var h3 = $('<h3 class="setting-title">Exterior/Износ</h3>');
+    $('.container').append(h3);
+    for (var key in dict_setting) {
+        var value = dict_setting[key];
+        // потом убрать это место, чтобы добавлять, убирать износ ALL в зависимости от магазина
+        if (value == exterior) {
+            var div = $('<div class="setting-bubble exterior on" id="' + key + '">' + value + '</div>');
+        }
+        else {
+            var div = $('<div class="setting-bubble exterior" id="' + key + '">' + value + '</div>');
+        }
+        $('.container').append(div);
+    }
 }
 
 function loadQualitySettingHTML(element) {
-    var quality_dict_html_id = {'quality_covert': 'Covert/Тайное', 'quality_classified': 'Classified/Засекреченное', 'quality_restricted': 'Restricted/Запрещенное', 'quality_milspec': 'Mil-Spec/Армейское', 'quality_industrial': 'Industrial/Промышленное', 'quality_consumer': 'Consumer/Ширпотреб'};
-    loadBubbleSettingFieldHTML(element, quality_dict_html_id);
+    var quality = FiltersValues.quality
+    var dict_setting = {'quality_covert': 'Covert/Тайное', 'quality_classified': 'Classified/Засекреченное', 'quality_restricted': 'Restricted/Запрещенное', 'quality_milspec': 'Mil-Spec/Армейское', 'quality_industrial': 'Industrial/Промышленное', 'quality_consumer': 'Consumer/Ширпотреб'};
+    $('.container').empty();
+    var h3 = $('<h3 class="setting-title">Quality/Качество</h3>');
+    $('.container').append(h3);
+    for (var key in dict_setting) {
+        var value = dict_setting[key];
+        // потом убрать это место, чтобы добавлять, убирать износ ALL в зависимости от магазина
+        if (value.includes(quality)) {
+            var div = $('<div class="setting-bubble quality on" id="' + key + '">' + value + '</div>');
+        }
+        else {
+            var div = $('<div class="setting-bubble quality" id="' + key + '">' + value + '</div>');
+        }
+        $('.container').append(div);
+    }
 }
 
 function loadCollectionSettingHTML() {
     $('.container').empty();
-
     if (FiltersValues.choose_collection == null) {
         var collections_json = window.FiltersSettingsJSON.collections_json
         $('.container').append($('<h3 class="setting-title">Collection</h3>'))
@@ -88,7 +116,7 @@ function loadCollectionSettingHTML() {
             industrial_wrap = $('<div class="flex-wrap">'),
             consumer_wrap = $('<div class="flex-wrap">');
         for (var i=0; i < collection_items.length; i++) {
-            item = collection_items[i]
+            var item = collection_items[i]
             if (item.quality == 'Covert') {
                 covert_wrap.append(loadItemBlockHTML(item));
             }
@@ -146,33 +174,73 @@ function chooseCollection (element) {
     element.textContent = '';
     $(element).append($('<img>', {src: './media/loading.gif'}));
 
-    var collection_id = element.value
-    var weapon_of_collection = []
-    var skins_json = window.FiltersSettingsJSON.weapons_skins_json
-    var collections_json = window.FiltersSettingsJSON.collections_json
+    var collection_id = element.value;
+    var weapon_of_collection = [];
+    var skins_json = window.FiltersSettingsJSON.weapons_skins_json;
+    var collections_json = window.FiltersSettingsJSON.collections_json;
     // get skins
     for (var i=0; i < skins_json.length; i++) {
-        var skin = skins_json[i]
-        if (skin.collection_id == collection_id) weapon_of_collection.push(skin)
+        var skin = skins_json[i];
+        if (skin.collection_id == collection_id) weapon_of_collection.push(skin);
     }
     // get collection
     for (var i=0; i < collections_json.length; i++) {
-        var collection = collections_json[i]
+        var collection = collections_json[i];
         if (collection.id == collection_id) {
-            FiltersValues.choose_collection = collection
+            FiltersValues.choose_collection = collection;
             break
-        }
-    
-    }
+        };
+    };
     FiltersValues.collection_weapons = weapon_of_collection
     loadCollectionSettingHTML()
+};
+
+function chooseExterior (element) {
+    var exterior = element.textContent;
+    FiltersValues.exterior = exterior;
+    var elements = document.getElementsByClassName('exterior');
+    for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
+        if (element.textContent == exterior) {
+            element.classList.add('on');
+        }
+        else {
+            if (element.className.includes('on')) {
+                element.classList.remove('on');
+           }
+        }
+    }
 }
+
+function chooseQuality (element) {
+    var quality = element.textContent.split('/')[0];
+    FiltersValues.quality = quality;
+    var elements = document.getElementsByClassName('quality');
+    for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
+        if (element.textContent.includes(quality)) {
+            element.classList.add('on');
+        }
+        else {
+            if (element.className.includes('on')) {
+                element.classList.remove('on');
+           }
+        }
+    }
+}
+
 
 function settingFieldEventHandler(event){
     var element = event.target
 
     if (element.tagName == 'BUTTON' && element.className == 'item-collection-button') {
-        chooseCollection (element)
+        chooseCollection(element);
+    }
+    else if (element.className.includes('setting-bubble') && element.className.includes('exterior')) {
+        chooseExterior(element);
+    }
+    else if (element.className.includes('setting-bubble') && element.className.includes('quality')) {
+        chooseQuality(element);
     }
 
     if (element.className == 'setting-bubble') {
@@ -191,17 +259,15 @@ function settingFieldEventHandler(event){
     }
 }
 
-
 function filterButtonsEventHandler(event){
     var element = event.target
-
-    blocks = document.getElementsByClassName('filter-block')
+    var blocks = document.getElementsByClassName('filter-block')
     for (var i = 0; i < blocks.length; i++) {
         var block = blocks[i];
         if (element.id == block.id) {
             element.classList.add('on');
             if (element.id == 'exterior_btn') {
-                loadExteriorSettingHTML(element);
+                loadExteriorSettingHTML();
                 }
             else if (element.id == 'quality_btn') {
                 loadQualitySettingHTML(element);
@@ -223,5 +289,4 @@ function filterButtonsEventHandler(event){
 
 document.getElementsByClassName('filter-column-block')[0].addEventListener('click', filterButtonsEventHandler)
 document.getElementsByClassName('container')[0].addEventListener('click', settingFieldEventHandler)
-
 //event.target.className.includes('on')
